345b27521dfa0a00c1ea8875a706b61560609aeb
janetanlien <janetanlien@gmail.com>
2021-12-04 14:57:08 +0800

    this is the latest commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>"Lorem ipsum dolor sit amet, nsint occaecat cupidorum."</Text>
+      <Text>"Hello world"</Text>
       <StatusBar style="auto" />
     </View>
   );
